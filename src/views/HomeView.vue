671baab24fdf90bb44c585d90a8d15a9eff85980
Breno-Coelho <brenocoelho004@gmail.com>
2025-03-14 19:41:09 -0300

    Primeiro commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,58 @@
+<script setup>
+    import HomeCard from '@/components/HomeCard.vue';
+</script>
+
+<template>
+
+    <div class="p-8">
+        <button
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Ouvir novamente
+        </button>
+
+        <div class="py-1.5"></div>
+
+        <div class="flex items-center">
+            <HomeCard image="https://i.pinimg.com/736x/10/00/2d/10002d10a9163b4b116e064558e44a77.jpg"  title="Aigis" subtitle="Aigis is the truth and the way"/>
+            <HomeCard image="https://i.pinimg.com/736x/6a/31/e0/6a31e0491b19006bcb20394867c0d141.jpg"  title="Fuuka" subtitle="Aigis is the truth and the way"/>
+            <HomeCard class="md:block" image="https://i.pinimg.com/736x/98/bd/6e/98bd6e1d7f87ad8a581c05e11a150be8.jpg"  title="Amada" subtitle="Aigis is the truth and the way"/>
+            <HomeCard class="lg:block" image="https://i.pinimg.com/736x/71/cc/75/71cc75ed005a5b89bd5df25f17ce8f98.jpg"  title="Elizabeth" subtitle="Aigis is the truth and the way"/>
+            <HomeCard class="xl:block" image="https://i.pinimg.com/736x/e1/59/72/e15972ead9b3f61a3a64391876abe56e.jpg"  title="Akihiko" subtitle="Aigis is the truth and the way"/>
+        </div>
+    </div>
+    <div class="p-8">
+        <button
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Continuar ouvindo
+        </button>
+
+        <div class="py-1.5"></div>
+
+        <div class="flex items-center">
+            <HomeCard image="https://i.pinimg.com/736x/10/00/2d/10002d10a9163b4b116e064558e44a77.jpg"  title="Aigis" subtitle="Aigis is the truth and the way"/>
+            <HomeCard image="https://i.pinimg.com/736x/6a/31/e0/6a31e0491b19006bcb20394867c0d141.jpg"  title="Fuuka" subtitle="Aigis is the truth and the way"/>
+            <HomeCard class="md:block" image="https://i.pinimg.com/736x/98/bd/6e/98bd6e1d7f87ad8a581c05e11a150be8.jpg"  title="Amada" subtitle="Aigis is the truth and the way"/>
+            <HomeCard class="lg:block" image="https://i.pinimg.com/736x/71/cc/75/71cc75ed005a5b89bd5df25f17ce8f98.jpg"  title="Elizabeth" subtitle="Aigis is the truth and the way"/>
+            <HomeCard class="xl:block" image="https://i.pinimg.com/736x/e1/59/72/e15972ead9b3f61a3a64391876abe56e.jpg"  title="Akihiko" subtitle="Aigis is the truth and the way"/>
+        </div>
+    </div> 
+    <div class="p-8">
+        <button
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Escolhas a dedo
+        </button>
+
+        <div class="py-1.5"></div>
+
+        <div class="flex items-center">
+            <HomeCard image="https://i.pinimg.com/736x/10/00/2d/10002d10a9163b4b116e064558e44a77.jpg"  title="Aigis" subtitle="Aigis is the truth and the way"/>
+            <HomeCard image="https://i.pinimg.com/736x/6a/31/e0/6a31e0491b19006bcb20394867c0d141.jpg"  title="Fuuka" subtitle="Aigis is the truth and the way"/>
+            <HomeCard class="md:block" image="https://i.pinimg.com/736x/98/bd/6e/98bd6e1d7f87ad8a581c05e11a150be8.jpg"  title="Amada" subtitle="Aigis is the truth and the way"/>
+            <HomeCard class="lg:block" image="https://i.pinimg.com/736x/71/cc/75/71cc75ed005a5b89bd5df25f17ce8f98.jpg"  title="Elizabeth" subtitle="Aigis is the truth and the way"/>
+            <HomeCard class="xl:block" image="https://i.pinimg.com/736x/e1/59/72/e15972ead9b3f61a3a64391876abe56e.jpg"  title="Akihiko" subtitle="Aigis is the truth and the way"/>
+        </div>
+    </div>        
+</template>
